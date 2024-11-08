d68644d887820f3e4aef3b0bfc66c230805ccff2
CCL95 <celsolarangeira@prof.educacao.sp.gov.br>
2024-11-08 14:33:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Astronomia',
+    'Qual o 4º planeta do sistema solar?',
+    'É o planeta Marte'
+)
+
+criaCartao(
+    'Física',
+    'Qual a característica do movimento retilíneo uniforme?',
+    'A velocidade é constante'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o símbolo do elemento mercúrio?',
+    'O símbolo do elemento mercúrio é Hg'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é o maior planeta do sistema solar?',
+    'Júpiter é o maior planeta do sistema solar '
+)
